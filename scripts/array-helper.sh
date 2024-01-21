83bf0e59a7a974e1f3b87ba59fe782c9b342a0ca
Mr. Rubber Ducky (Simon) <79613254+MrRubberDucky@users.noreply.github.com>
2024-01-21 07:40:47 +0100

    Update array-helper.sh
    Signed-off-by: Mr. Rubber Ducky (Simon) <79613254+MrRubberDucky@users.noreply.github.com>

--- scripts/array-helper.sh
+++ scripts/array-helper.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-# https://unix.stackexchange.com/a/403401
 source /app/.bashrc
 
-# Read lines from .MODULES into shell array $lines[array number]
-mapfile -t lines < /app/scripts/.MODULES
-
-# Create the command
-# Beginning - Start of the command
+mapfile -t lines < /app/helper/.MODULES
 cmd_array=( xcaddy build v${CADDY_VERSION} )
-# Middle For loop (ex. --with $module[1] --with $module[2] etc.), repeats as long as there's content in the file
+
 for module in "${lines[@]}"; do
     cmd_array+=( --with "$module" )
 done
-# End
-cmd_array+=( --output /app/caddy )
 
-# Involve the command
+cmd_array+=( --output /usr/bin/caddy )
 "${cmd_array[@]}"
+
+# https://unix.stackexchange.com/a/403401
