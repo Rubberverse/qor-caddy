04acea88ae128f91409af8fbb87cdd3d44455e1f
Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>
2025-01-10 15:12:55 +0100

    Update pinning for corazawaf/coraza-caddy to commit f1f7ea
    Signed-off-by: Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>

--- scripts/array-helper.sh
+++ scripts/array-helper.sh
@@ -66,7 +66,7 @@ mv $TEMP_FILE $FILE_PATH
 printf "\n[array-helper] Pinning Caddy version according to tag, commit or branch\n"
 go get github.com/caddyserver/caddy/v2@${GO_CADDY_VERSION}
 printf "\n[array-helper] Pinning corazawaf/coraza-caddy version to commit 2502703e84440c48f4c044e5a88c0c036315fd40 on main branch\n"
-go get github.com/corazawaf/coraza-caddy/v2@2502703e84440c48f4c044e5a88c0c036315fd40
+go get github.com/corazawaf/coraza-caddy/v2@f1f7ea41bf9113b2b3e7f2c3afe4079e9d811b77
 
 printf "[array-helper] Running go mod tidy to add module requirements and create go.sum\n"
 go mod tidy
