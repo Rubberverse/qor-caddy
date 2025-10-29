4f58de65d14901d757e4eb0390a388007e2bc3c3
Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>
2025-10-29 11:00:45 +0100

    Update array-helper.sh
    Signed-off-by: Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>

--- scripts/array-helper.sh
+++ scripts/array-helper.sh
@@ -63,8 +63,15 @@ cat $FILE_PATH
 
 printf "\n[array-helper] Pinning Caddy version according to tag, commit or branch\n"
 go get github.com/caddyserver/caddy/v2@${GO_CADDY_VERSION}
-printf "[array-helper] Running go mod tidy to add module requirements and create go.sum\n"
+printf "\n[array-helper] Pulling caddy-defender\n"
+git clone https://github.com/JasonLovesDoggo/caddy-defender.git caddy-defender
+printf "\n[array-helper] Append local path for caddy-defender\n"
+echo 'replace pkg.jsn.cam/caddy-defender => ./caddy-defender' >> go.mod
+printf "\n[array-helper] Running go mod tidy to add module requirements and create go.sum\n"
 go mod tidy
+printf "\n[array-helper] Adding extra ASNs for caddy-defender\n"
+printf "\n[array-helper] OVH,Tencent,Scaleway,Scala,Namecheap,LiquidWeb,Linode,IONOS,Hostinger,Hetzner,DigitalOcean,Contabo\n"
+go run ./caddy-defeder/ranges/main.go --fetch-tor --asn "AS16276,AS35540,AS45090,AS12876,AS29447,AS54265,AS40476,AS22612,AS32244,AS6188,AS22558,AS40819,AS53824,AS201682,AS63949,AS48337,AS8560,AS51862,AS47583,AS204915,AS24940,AS213230,AS212317,AS14061,AS39690,AS62567,AS133165,AS135340,AS200130,AS201229,AS202018,AS202109,AS205301,AS393406,AS394362,AS40021,AS51167,AS141995"
 printf "[array-helper] Continuing with build process\n"
 
 # https://unix.stackexchange.com/a/403401
