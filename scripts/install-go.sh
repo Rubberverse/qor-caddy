6dd1d1dbd5b9fb021786ebca1a8fa3cc92606f1f
Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>
2026-01-13 05:36:08 +0100

    Update install-go.sh to be multi-arch. at least a bit.
    Signed-off-by: Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export GO_VERSION=$(curl https://go.dev/dl/?mode=json | jq -r '.[0].version' )
-curl -Lo golang.tar.gz go.dev/dl/${GO_VERSION}.linux-amd64.tar.gz
+dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
+curl -Lo golang.tar.gz https://go.dev/dl/${GO_VERSION}.linux-${dpkgArch}.tar.gz
 tar -C /usr/local -xzf golang.tar.gz
 rm golang.tar.gz
 git config --global --add safe.directory '*'
