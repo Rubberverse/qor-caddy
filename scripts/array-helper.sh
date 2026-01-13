59f62db2c40e7bd3ada82ea93b45748b2e1b395a
Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>
2026-01-13 06:49:33 +0100

    bruh it's exactly same.
    Signed-off-by: Mr. Rubber Ducky <79613254+MrRubberDucky@users.noreply.github.com>

--- scripts/array-helper.sh
+++ scripts/array-helper.sh
@@ -26,13 +26,13 @@ read -ra CADDY_MODULES_ARRAY <<< "${CADDY_MODULES}"
 echo -n "" > $TEMP_FILE
 
 if [[ ${DEBUG} ]]; then
-    printf "%b" "\n[DBG] Listing variables and file directories...\n"
-    printf "%b" "\n[DBG] File Path: ${FILE_PATH}\n"
-    printf "%b" "\n[DBG] Temporary File: ${TEMP_FILE}\n"
-    printf "%b" "\n[DBG] Was Array processed? - ${PROCESSED}\n"
-    printf "%b" "\n[DBG] First Value in Array - ${CADDY_MODULES_ARRAY}\n"
-    printf "%b" "\n[DBG] Modules: ${CADDY_MODULES}\n"
-    printf "%b" "\n[DBG] Caddy Version: ${GO_CADDY_VERSION}\n"
+        printf "%b" "\n[DBG] Listing variables and file directories...\n"
+        printf "%b" "\n[DBG] File Path: ${FILE_PATH}\n"
+        printf "%b" "\n[DBG] Temporary File: ${TEMP_FILE}\n"
+        printf "%b" "\n[DBG] Was Array processed? - ${PROCESSED}\n"
+        printf "%b" "\n[DBG] First Value in Array - ${CADDY_MODULES_ARRAY}\n"
+        printf "%b" "\n[DBG] Modules: ${CADDY_MODULES}\n"
+        printf "%b" "\n[DBG] Caddy Version: ${GO_CADDY_VERSION}\n"
 fi
 
 sed -i -e "s|// plug in Caddy modules here|_ \"github.com/caddyserver/caddy/v2\"|g" $FILE_PATH
@@ -50,14 +50,14 @@ do
     echo "$line" >> $TEMP_FILE
 done < "$FILE_PATH"
 
-printf "[array-helper] Overwriting main.go with temporary file...\n"
+printf "%b" "\n[array-helper] Overwriting main.go with temporary file...\n"
 mv -f $TEMP_FILE $FILE_PATH
 
 if [[ ${DEBUG} ]]; then
         cat $FILE_PATH
 fi
 
-printf "\n[array-helper] Pinning Caddy version according to tag, commit or branch"
+printf "%b" "\n[array-helper] Pinning Caddy version according to tag, commit or branch\n"
 go get github.com/caddyserver/caddy/v2@${GO_CADDY_VERSION}
 
 if [[ ${CADDY_DEFENDER} ]]; then
@@ -66,7 +66,7 @@ if [[ ${CADDY_DEFENDER} ]]; then
         echo 'replace pkg.jsn.cam/caddy-defender => ./caddy-defender' >> go.mod
 fi
 
-printf "\n[array-helper] Running go mod tidy to add module requirements and create go.sum\n"
+printf "%b" "\n[array-helper] Running go mod tidy to add module requirements and create go.sum\n"
 go mod tidy
 
 if [[ ${CADDY_DEFENDER}} ]] && [[ ${ASN_RANGES} ]]; then
